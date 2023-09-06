f628b2d28ad0c5b8dae333a634b965102b37b460
PedroHenriqueLOURENCO <rm98402@fiap.com.br>
2023-09-06 15:08:16 -0300

    Configurando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -13,3 +13,25 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element: <Produtos/>
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProdutos/>
+      }
+    ]
+  }
+])
